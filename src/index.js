2b030d35a8afe3148b523845e607ee51d3b2254d
l-AngelMoya <langel_andiyaro@hotmail.com>
2023-01-16 08:26:01 -0500

    This is a test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import app from "./app.js"
-import dotenv from "dotenv"
-import { sequelize } from "../lib/sequelize.js"
+import app from "./app.js";
+import dotenv from "dotenv";
+import { sequelize } from "../lib/sequelize.js";
 
 dotenv.config();
 
 async function main() {
   try {
-    await sequelize.authenticate()
-    console.log("Connection has been established successfully") 
-    app.listen(process.env.PORT || 3000)
-    console.log(`Sever is listening on port ${process.env.PORT}`)
-  }catch(err){
-    console.error("Unable to connect to the database:", err.message)
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully");
+    app.listen(process.env.PORT || 3000);
+    console.log(`Sever is listening on port ${process.env.PORT}`);
+    console.log("This is a test");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err.message);
   }
 }
 
-main()
+main();
